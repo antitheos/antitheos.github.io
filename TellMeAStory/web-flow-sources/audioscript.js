fa6f2b16cdc8fce7b9ccfbf0309a78a211ef1fd8
Ade Ayoade <antitheos@gmail.com>
2020-09-16 19:21:04 -0400

    updates to wait for jquery

--- TellMeAStory/web-flow-sources/audioscript.js
+++ TellMeAStory/web-flow-sources/audioscript.js
@@ -1,13 +1,20 @@
-$.ajax({
-    url: 'https://antitheos.github.io/TellMeAStory/web-flow-sources/themes.json',
-    dataType: 'json',
-    success: function (data) {
-        var el = $("#theme-selector");
-        for (var x in data) {
-            console.log(data[x]);
-            var op = el.add("option");
-            op.text(data[x]);
+document.addEventListener("DOMContentLoaded", windowReady);
 
-        }
+function windowReady() {
+    if ($ == null || $ == undefined) {
+        setTimeout(windowReady, 1000);
+        return
     }
-});
+
+    $.ajax({
+        url: 'https://antitheos.github.io/TellMeAStory/web-flow-sources/themes.json',
+        dataType: 'json',
+        success: function (data) {
+            var el = $("#theme-selector");
+            for (var x in data) {
+                el.append("<option>" + data[x] + "</option>");
+
+            }
+        }
+    });
+}
