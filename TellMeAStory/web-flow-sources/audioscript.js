2831d3da8be6e36ea4ef48992125175887b85bc4
Ade Ayoade <antitheos@gmail.com>
2020-09-16 19:41:46 -0400

    fixing items

--- TellMeAStory/web-flow-sources/audioscript.js
+++ TellMeAStory/web-flow-sources/audioscript.js
@@ -9,13 +9,14 @@ function windowReady() {
 
     $.ajax({
         url: 'https://antitheos.github.io/TellMeAStory/web-flow-sources/themes.json',
-        dataType: 'json',
-        success: function (data) {
-            var el = $("#theme-selector");
-            for (var x in data) {
-                el.append("<option>" + data[x] + "</option>");
+        dataType: 'json'
+    }).done(function (data) {
+        console.log(data)
+        var el = $("#theme-selector");
+        for (var x in data) {
+            el.append("<option>" + data[x] + "</option>");
 
-            }
         }
     });
+
 }
