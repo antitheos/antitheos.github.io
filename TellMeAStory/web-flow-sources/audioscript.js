4d8c9f956702a3017d6fe7c26eca5e02d647433c
Ade Ayoade <antitheos@gmail.com>
2020-09-16 19:07:27 -0400

    Update audioscript.js

--- TellMeAStory/web-flow-sources/audioscript.js
+++ TellMeAStory/web-flow-sources/audioscript.js
@@ -1,19 +1,13 @@
 $.ajax({
-    url: 'https://spreadsheets.google.com/feeds/list/1DRi3DjB8YC2AURscSgNhfSEhEdQb3Ehh-5uIaR-CmDo/1/public/values?alt=json-in-script',
-    dataType: 'jsonp',
-    success: function (dataWeGotViaJsonp) {
-        var ls = dataWeGotViaJsonp.feed.entry;
-        for (var i in ls) {
-            var p = ls[i];
-            var person = p.gsx$participant.$t;
-            if (partipants[person] == null) {
-                partipants[person] = {
-                    personName: person,
-                    color: p.gsx$color.$t,
-                    mapObjects: []
-                };
-            }
+    url: 'https://antitheos.github.io/TellMeAStory/web-flow-sources/themes.json',
+    dataType: 'json',
+    success: function (data) {
+        var el = $("#theme-selector");
+        for (var x in data) {
+            console.log(data[x]);
+            var op = el.add("option");
+            op.text(data[x]);
+
         }
-        participantsReady = true;
     }
 });
