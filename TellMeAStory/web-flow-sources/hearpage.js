3f536b9f2b58fdb30846a195b6efea8abb6bb364
Ade Ayoade <antitheos@gmail.com>
2020-09-16 20:04:45 -0400

    Create hearpage.js

---
+++ TellMeAStory/web-flow-sources/hearpage.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", windowReady);
+
+function windowReady() {
+    if ($ == null || $ == undefined) {
+        setTimeout(windowReady, 1000);
+        return
+    }
+    console.log("fetch data")
+
+    $.ajax({
+        url: 'https://antitheos.github.io/TellMeAStory/web-flow-sources/themes.json',
+        dataType: 'json'
+    }).done(function (data) {
+        console.log(data)
+        var el = $("#theme-selector");
+        for (var x in data) {
+            el.append("<option value='" + data[x] + "'>" + data[x] + "</option>");
+
+        }
+    });
+
+}
