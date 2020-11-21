e1c74e7b454e97bb2301bcfb3239551162d5e7aa
Ade Ayoade <antitheos@gmail.com>
2020-11-20 22:43:02 -0500

    update to play collection

--- TellMeAStory/web-flow-sources/hear/script.js
+++ TellMeAStory/web-flow-sources/hear/script.js
@@ -58,10 +58,37 @@ function selectRandomTheme() {
     themeSelected($("#theme-selector"))
 }
 
+
+function playCollection(val) {
+    var themes = collection[val].map(e => e.toLowerCase().trim());
+    var fh = themes.pop();
+    var data = themesData[fh].list;
+    data = data.filter(function (e) {
+        var ethemes = e.themes.map(e => e.key);
+        for (var th in themes) {
+            if (!ethemes.find(e => e == th)) {
+                return false;
+            }
+        }
+        return true;
+    })
+
+
+
+}
+
 function showRelatedStory(e) {
     var themes = $(e).data("themes");
     var playList = $(e).data("playList").slice(); //randomize order 
 
     showStory(themes, playList, false);
 
+}
+
+var collection = {
+    "How can we center women in the history of HIV/AIDS?": ["Knowledge about HIV/AIDS", "Contracting HIV/AIDS from a male partner", "In the streets", "HIV saved my life", "Future"],
+    "How does family matter in the history of HIV / AIDS ?": ["Childhood", "Migrations", "Trauma", "Talking about HIV in families", "Children"],
+    "How do institutions impact women living with HIV / AIDS ?": ["Schooling", "Nowhere to go", "Incarceration", "Surveillance", "Women’ s Interagency HIV Study"],
+    "How is health more than the absence of disease ?": ["Partnerships", "Addiction and recovery", "Getting and giving support", "Living with HIV", "Healing"],
+    "How do women build healthy worlds ?": ["Healthcare", "Family dynamics", "Taking charge of health and wellness", "Work", "Religion and spirituality"],
 }
