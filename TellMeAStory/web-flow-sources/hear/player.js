b3a19a214a3210ac7a36c8ff0445c1a47f8d7801
Ade Ayoade <antitheos@gmail.com>
2020-11-25 12:28:08 -0500

    Update player.js

--- TellMeAStory/web-flow-sources/hear/player.js
+++ TellMeAStory/web-flow-sources/hear/player.js
@@ -319,7 +319,7 @@ function loadNowPlayingThemes(themes) {
         });
 
 
-        if (!collectionThemes && (currentThemes.length > 1 || currentThemes.data[theme.key])) {
+        if (!collectionThemes && (currentThemes.length > 1 || !currentThemes.data[theme.key])) {
             var item = template.clone();
             related.append(item);
             $(item).text(theme.text);
